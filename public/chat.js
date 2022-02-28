a05bb0e3212c334fbf333f4b4b3266bf6497c4ad
hector <elmejordx06@gmail.com>
2022-02-28 09:42:11 -0400

    tratando el one to one

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,8 @@
 
 
 const socket = io ();
-
+let userId = document.getElementById('userid');
+let btnId = document.getElementById('user-id')
 let massage = document.getElementById('message');
 let username = document.getElementById('username');
 let btn = document.getElementById('send');
@@ -9,33 +10,32 @@ let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
 
-btn.addEventListener('click', function (e) {
+btnId.addEventListener('click', function (e) {
   
-  socket.emit('chat:message',{
-    massage: massage.value,
-    username: username.value,
-  } 
-
-  )
+  socket.emit('userid',  userId.value)
+  console.log(`Soy el Usuario con el Id ${userId.value} y Mi socket Id Es ${socket.id}`);
   
 });
+
+ 
+btn.addEventListener('click', function (e) {
+  
+  
+  socket.emit('chat:message',{
+    senderId: userId,
+    receiverId,
+    massage: massage.value,
+    username: username.value,
+    })  
+});
+
+ 
 
 massage.addEventListener('keypress', function () {
   socket.emit('chat:typing', username.value);
 });
 
-socket.on("connect", () => {  
-  console.log(socket.id);
- });
 
-
-
- socket.on('privatemessage', function (anotherSocketId, msg) {
-  actions.innerHTML = '';
- output.innerHTML +=`<p>
-   <strong>${anotherSocketId}</strong>: ${msg}
-  </p>`
-})
 
 socket.on('chat:message', function (data) {
    actions.innerHTML = '';
@@ -45,13 +45,8 @@ socket.on('chat:message', function (data) {
 })
 
 socket.on('chat:typing', function (data) {
-  
   actions.innerHTML +=`<p><em>${data} is typing a Message</em></p>`
   
 })
 
-socket.on('user-connected', function (data) {
-  actions.innerHTML +=`<p><em>${data.username} Is Connected</em></p>`
-  
-})
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { use } = require('express/lib/application');
 const app = express();
 const path = require('path')
 
@@ -14,16 +15,41 @@ const server = app.listen(3000, ()=>{
 const SocketIO = require('socket.io');
 const io = SocketIO(server);
 
-const users = {}
+const users = []
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) && users.push({userId, socketId});
+};
+
+const removeUser = (socketId) =>{
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 
 
 io.on('connection', (socket) => {
   console.log(`El Usuario ${socket.id} Se Ha Connectado`)
  
+   socket.on('userid', function(userId) {
+     addUser(userId, socket.id);
+     io.emit('getUsers', users);
+     console.log(`Soy el Usuario con el ID ${userId} y Mi Socket Id Es ${socket.id}`)
+     console.log(users);
+   });
+  
 
-
-  socket.on('chat:message', (data) =>{
-   io.sockets.emit('chat:message', data)
+  socket.on('chat:message', ({senderId, receiverId, message, username}) =>{
+    const user = getUser(receiverId);
+   
+   io.to(user).emit('chat:messge', {
+     senderId,
+     username,
+     message
+   })
+   console.log(data)
   })
 
   socket.on('chat:typing', (data) => {
